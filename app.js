7ad874034eff5d46f632cb691cc9b86d9767abb3
MarzanaN <m.nahar@live.co.uk>
2025-06-05 12:12:58 +0100

    fixed linkedin view

--- app.js
+++ app.js
@@ -142,3 +142,9 @@ if (!isHoverable) {
   });
 }
 
+function setVhUnit() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', setVhUnit);
+setVhUnit();
